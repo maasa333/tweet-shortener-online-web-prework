5b82ad7302cbdf0897ac0130b4dd4e8172c49163
Maasa Kono <konomaasa@gmail.com>
2019-05-21 02:46:31 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+dictionary = {
+  "hello" => "hi",
+  "to" => "2",
+  "two" => "2",
+  "too" => "2",
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
+
+def word_substitutor(tweet_one)
+  tweet_one.split(" ").map do |original_w|
+    original_w
+  end
+end
