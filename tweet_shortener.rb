83b27e3df1721403c3e348786c11e6dc1507a734
Maasa Kono <konomaasa@gmail.com>
2019-05-23 13:44:13 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,18 +1,26 @@
-dictionary = {
-  "hello" => "hi",
-  "to" => "2",
-  "two" => "2",
-  "too" => "2",
-  "for" => "4",
-  "four" => "4",
-  "be" => "b",
-  "you" => "u",
-  "at" => "@",
-  "and" => "&"
-}
+require 'pry'
 
-def word_substitutor(tweet_one)
-  tweet_one.split(" ").map do |original_w|
-    original_w
-  end
+def dictionary
+  dictionary = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet_one)
+  tweet_one.split.map do |original_w|
+    if dictionary.keys.include?(original_w.downcase)
+      original_w = dictionary[original_w.downcase]
+    else
+      original_w
+    end
+  end.join(" ")
 end
